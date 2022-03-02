9f5a3b518586715257dd6782f9b348b0ecddc0df
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-01 19:58:20 -0500

    Reversing a string, functionaly complete. Still need to have console.log as one log and not multiple

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 "use strict"
 // 1. Reversing a string
+// First I need to create a function that takes in a string. 
+// Second I need to figure out how to reverse the string.
+// Third I need to return the reversed string.
+let string1 = "abcdefghijklmnopqrstuvwxyz";
+let returnString;
+function ReverseString (string){
+    for (let i=string.length - 1; i>= 0; i--){
+        console.log(string[i]);
 
+    }
+};
+ReverseString(string1);
 
 
 // 2. Capitalize a letter
