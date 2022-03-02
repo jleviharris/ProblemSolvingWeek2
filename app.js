ed40e63e95b70f2e55cbc3000df3fb68548b3a16
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-01 22:17:31 -0500

    Reworked problems with prompt added in. Also fixed problem 2 so that it also capitalized the very first letter.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // First I need to create a function that takes in a string. 
 // Second I need to figure out how to reverse the string.
 // Third I need to return the reversed string.
-let string1 = "abcdefghijklmnopqrstuvwxyz";
+let string1 = prompt("What word would you like reversed?");
 let returnString = [];
 function ReverseString (string){
     for (let i=string.length - 1; i>= 0; i--){
@@ -17,11 +17,13 @@ ReverseString(string1);
 //Second I need to target every letter that comes after a space.
 //Third Ill need to change the targeted letters to uppercase.
 // Last thing is ill need to return the new string.
-let string2 = "I want all the first letters to be capatalied";
+let string2 = prompt("I will capitalize all the first letters for you!");
 let returnString2 = [];
 function Capitalize (string){
     for (let i=0; i<string.length; i++){
-        if (string[i-1]=== " " ){  
+        if ( i === 0){
+            returnString2.push(string[i].toUpperCase())
+        }else if (string[i-1] === " "){  
             returnString2.push(string[i].toUpperCase())
         }else returnString2.push(string[i]);
     };
@@ -31,13 +33,15 @@ Capitalize(string2);
 
 
 // 3. Compress a string of characters
+//First I need to create a function that takes in a string
+//Next 
 
 
 
 // Bonus challenge: Palindrome 
 // For this problem Im thinking that Ill be able to use problem one and rework it so that 
 // once it runs the function from the first problem ill check to see if it equalls the original string.
-let string3 = "madam"
+let string3 = prompt("What string would you like to check");
 let returnString3 = "";
 function ReverseString3 (string){
     for (let i=string.length - 1; i>= 0; i--){
