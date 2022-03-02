0e1970e9be8a71b381f9bf7eb9eff6cdf0d7175b
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-01 21:25:25 -0500

    Completed problem 2

--- app.js
+++ app.js
@@ -8,15 +8,26 @@ let returnString = [];
 function ReverseString (string){
     for (let i=string.length - 1; i>= 0; i--){
         returnString.push(string[i]);
-
     };
     console.log(returnString);
 };
 ReverseString(string1);
-
-
 // 2. Capitalize a letter
-
+// First I need to create a function that takes in a string.
+//Second I need to target every letter that comes after a space.
+//Third Ill need to change the targeted letters to uppercase.
+// Last thing is ill need to return the new string.
+let string2 = "I want all the first letters to be capatalied";
+let returnString2 = [];
+function Capitalize (string){
+    for (let i=0; i<string.length; i++){
+        if (string[i-1]=== " " ){  
+            returnString2.push(string[i].toUpperCase())
+        }else returnString2.push(string[i]);
+    };
+    console.log(returnString2);
+};
+Capitalize(string2);
 
 
 // 3. Compress a string of characters
@@ -24,3 +35,21 @@ ReverseString(string1);
 
 
 // Bonus challenge: Palindrome 
+// For this problem Im thinking that Ill be able to use problem one and rework it so that 
+// once it runs the function from the first problem ill check to see if it equalls the original string.
+let string3 = "madam"
+let returnString3 = [];
+function ReverseString3 (string){
+    for (let i=string.length - 1; i>= 0; i--){
+        returnString3.push(string[i]);
+    };
+    console.log(returnString3);
+};
+
+
+function Palindrome (string){
+    if (ReverseString3(string) === string){
+        console.log('Palindrome')
+    }else console.log('Failed Test')
+};
+Palindrome(string3);
